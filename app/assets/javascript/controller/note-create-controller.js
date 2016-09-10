1288f0cf44764bbbc5afba8c297d0af2127bef36
anujparikh <anujparikh1@gmail.com>
2016-09-10 19:02:13 -0400

    note functionality

--- app/assets/javascript/controller/note-create-controller.js
+++ app/assets/javascript/controller/note-create-controller.js
@@ -5,7 +5,19 @@
 
     var vm = this;
 
-    function NoteCreateController() {
+    function NoteCreateController($scope, Note, $location) {
+        $scope.note = new Note();
+        $scope.isSubmitting = false;
 
+        $scope.saveNote = function (note) {
+            $scope.isSubmitting = true;
+            note.$save()
+                .then(function () {
+                    $location.path('/notes');
+                })
+                .finally(function () {
+                    $scope.isSubmitting = false;
+                })
+        }
     }
 })();
--- app/assets/javascript/controller/note-show-controller.js
+++ app/assets/javascript/controller/note-show-controller.js
@@ -3,7 +3,13 @@
         .module('NoteWrangler')
         .controller('NoteShowController', NoteShowController);
 
-    function NoteShowController(Note, $scope, $routeParams) {
+    function NoteShowController(Note, $scope, $routeParams, $location) {
         $scope.note = Note.get({id: $routeParams.id});
+
+        $scope.deleteNote = function (note) {
+            note.$remove().then(function() {
+                $location.path('/notes')
+            });
+        }
     }
 })();
